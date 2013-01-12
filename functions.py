51128cc0e7bf768b89fad6af4283d2fdc4517c32
Jose Luis Jaime <joseluis.jaime.g@gmail.com>
2013-01-13 00:51:25 +0100

    add a check functions

--- functions.py
+++ functions.py
@@ -1,5 +1,20 @@
 from subprocess import call
+import commands
+import sys
 
+def checkusername(username):
+    listuser = commands.getoutput("ls /srv/hosting/")
+    listuser = listuser.split("\n")
+    if username in listuser:
+        print "Username in use"
+        sys.exit()
+
+def checkdomainname(domainname):
+    listdomain = commands.getoutput("ls /etc/apache2/sites-available/")
+    listdomain = listdomain.split("\n")
+    if domainname in listdomain:
+        print "domain in use"
+        sys.exit()
 
 def createdir(username):
     call(["mkdir","-p","/srv/hosting/" + username])
@@ -17,3 +32,6 @@ def createvh(username,domainname):
     nuevo = open("/etc/apache2/sites-available/"+domainname,"w")
     nuevo.write(texto)
     nuevo.close() 
+
+    call(["a2ensite",domainname])
+    call(["service","apache2","reload"])
--- newhosting.py
+++ newhosting.py
@@ -1,7 +1,9 @@
 import functions as fun
 
 username = raw_input("enter your username: ")
+fun.checkusername(username)
 domainname = raw_input("enter your domain name, without www and .com :")
+fun.checkdomainname(domainname)
 
 fun.createdir(username)
 fun.createvh(username,domainname)
