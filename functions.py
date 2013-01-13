b2f7462542bc4c92b523a330ee32b9f02cc98e77
Jose Luis Jaime <joseluis.jaime.g@gmail.com>
2013-01-13 13:28:34 +0100

    functions to create ldap user and generate aleatory pass

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 from subprocess import call
 import commands
 import sys
+from random import choice
+import os
+import hashlib
+from base64 import urlsafe_b64encode as encode
+import ldap
+import ldap.modlist as modlist
 
 def checkusername(username):
     listuser = commands.getoutput("ls /srv/hosting/")
@@ -21,7 +27,6 @@ def createdir(username):
     call(["chmod","777","/srv/hosting/" + username])
     call(["cp","src/index.html","/srv/hosting/" + username])
 
-
 def createvh(username,domainname):
     f = open("src/default", "r")
     texto = f.read()
@@ -35,3 +40,43 @@ def createvh(username,domainname):
 
     call(["a2ensite",domainname])
     call(["service","apache2","reload"])
+
+def aleatorypassword():
+	
+	length = 7
+	string = "1234567890abcdefghijklmnopqrstuvwxyz"
+	password = ""
+	password = password.join([choice(string) for i in range(length)])
+	return password
+	
+def encryptpassword(password):
+	
+	salt = os.urandom(4)
+	h = hashlib.sha1(password)
+	h.update(salt)
+	return "{SSHA}" + encode(h.digest() + salt)
+	
+def adduserldap(username,domainname,passencrypt,uidnumber):
+	l = ldap.initialize("ldap://debian.example.com")
+    l.simple_bind_s("cn=admin,dc=example,dc=com","usuarioq")
+    
+    dn="uid=%s,ou=People,dc=example,dc=com" %username
+    
+    attrs = {}
+    attrs['objectClass'] = ['Top','posixAccount','account']
+    attrs['cn'] =username
+    attrs['uid'] =username
+    attrs['uidNumber'] = uidnumber
+    attrs['gidNumber'] = '2002'
+    attrs['homeDirectory'] = '/srv/hosting/%s' %username
+    attrs['userPassword'] = passencrypt
+    attrs['loginShell'] = '/bin/false'
+    attrs['description'] = domainname
+    
+    ldif = modlist.addModlist(attrs)
+    
+    l.add_s(dn,ldif)
+    
+    l.unbind_s
+    
+    
