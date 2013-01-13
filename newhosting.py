834accae1d95f3162f50be2ce8e4dfb46007bbcf
Jose Luis Jaime <joseluis.jaime.g@gmail.com>
2013-01-13 15:04:41 +0100

    update newhosting with the new functions

--- newhosting.py
+++ newhosting.py
@@ -7,3 +7,16 @@ fun.checkdomainname(domainname)
 
 fun.createdir(username)
 fun.createvh(username,domainname)
+
+passldap = fun.aleatorypassword()
+
+enpassldap = fun.encryptpassword(passldap)
+
+lastuid = fun.lastuid()
+
+fun.adduserldap(username,domainname,enpassldap,lastuid)
+
+print "---------------------------------------------"
+print "tu usuario para entrar en ftp es: " + username
+print "Tu nombre de dominio es: www." +domainname + ".com"
+print "tu password ftp es: " + passldap
